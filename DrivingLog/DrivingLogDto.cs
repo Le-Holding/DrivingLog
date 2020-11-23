844de10f6a592aee38ba3337253ef7e40f2f5dda
Mathias Casper Lynge Le-Holding <talamias@hotmail.com>
2020-11-23 08:59:24 +0100

    add merge conflicts

---
+++ DrivingLog/DrivingLogDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DrivingLog
+{
+  internal class DrivingLogDto
+  {
+    public DateTime Date { get; set; }
+    public string Task { get; set; }
+    public int Distance { get; set; }
+  }
+}
--- DrivingLog/Form1.cs
+++ DrivingLog/Form1.cs
@@ -14,18 +14,20 @@ namespace DrivingLog
   {
     List<StamdataDto> _personer = new List<StamdataDto>()
       {
-        new StamdataDto { Id = 1, Name = "Steve", killometer = 1000 },
-        new StamdataDto { Id = 2, Name = "Daniel", killometer = 256 },
-        new StamdataDto { Id = 3, Name = "Jesper", killometer = 98 },
-        new StamdataDto { Id = 4, Name = "Sara", killometer = 56 }
+        new StamdataDto { Id = 1, Name = "Steve", killometer = 1000, LicensePlate = "CP86028", Date = new DateTime(2020, 08, 1)},
+        new StamdataDto { Id = 2, Name = "Daniel", killometer = 256, LicensePlate = "AR86028", Date = new DateTime(2020, 01, 1) },
+        new StamdataDto { Id = 3, Name = "Jesper", killometer = 98, LicensePlate = "BK08022", Date = new DateTime(2019, 05, 25) },
+        new StamdataDto { Id = 4, Name = "Sara", killometer = 56, LicensePlate = "BA09455", Date = new DateTime(2015, 12, 30) }
       };
+
+    List<DrivingLogDto> drivingLogs = new List<DrivingLogDto>();
 
     public Form1()
     {
       InitializeComponent();
 
       dataGridView1.DataSource = /*new BindingSource8*/new BindingList<StamdataDto>(_personer)/*, $"")*/;
-      
+
       dataGridView1.Columns[$"{nameof(StamdataDto.Id)}"].Visible = false;
 
       DataGridViewButtonColumn Delete = new DataGridViewButtonColumn()
@@ -45,7 +47,7 @@ namespace DrivingLog
 
       this.dataGridView1.Columns.Add(Delete);
       this.dataGridView1.Columns.Add(Edit);
-;
+      ;
 
 
       //int columnIndex = 3;
@@ -63,7 +65,7 @@ namespace DrivingLog
       if (e.ColumnIndex == dataGridView1.Columns["Edit_column"].Index)
       {
         Form2 view = new Form2();
-        
+
         if (view.ShowDialog(this) == DialogResult.OK)
         {
           //var t = dataGridView1.SelectedRows
@@ -72,13 +74,13 @@ namespace DrivingLog
         view.Dispose();
         //Do something with your button.
       }
-      if(e.ColumnIndex == dataGridView1.Columns["Delete_column"]?.Index)
+      if (e.ColumnIndex == dataGridView1.Columns["Delete_column"]?.Index)
       {
         //ColorDialog m = new ColorDialog();
-        var message = "Ønsker du at slette rækken?";
+        var message = string.Format("Ønsker du at slette brugeren {0}?", this.dataGridView1.Rows[e.RowIndex].Cells);
         var Caption = "Delete";
         var result = MessageBox.Show(message, Caption, MessageBoxButtons.OKCancel);
-        if(result == DialogResult.OK)
+        if (result == DialogResult.OK)
         {
           //var row = this.dataGridView1.SelectedRows[e.RowIndex];
 
