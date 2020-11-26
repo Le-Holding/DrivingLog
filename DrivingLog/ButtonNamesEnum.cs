7951f427ee0fb919db94b8da0919083d2f0ebd26
Mathias Casper Lynge Le-Holding <talamias@hotmail.com>
2020-11-26 08:22:26 +0100

    add ButtonNamesEnum

---
+++ DrivingLog/ButtonNamesEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrivingLog.Properties
+{
+  public enum ButtonNamesEnum
+  {
+    Add_column = 100,
+    Edit_column = 101,
+    Delete_column = 102
+  }
+}
