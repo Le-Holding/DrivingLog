32975b7bc75d0a30398ce34c750d90e17ffc96d4
Mathias Casper Lynge Le-Holding <talamias@hotmail.com>
2020-11-27 12:19:47 +0100

    add test commit

--- DrivingLog/Program.cs
+++ DrivingLog/Program.cs
@@ -17,6 +17,8 @@ namespace DrivingLog
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new Form1());
+
+      // Hej
     }
   }
 }
