06c2ba586eecd47e241902247172691656989ff3
Mathias Casper Lynge Le-Holding <talamias@hotmail.com>
2021-01-18 11:10:38 +0100

    add class dataAccessLayer

---
+++ DrivingLog/Repository/DataAccessLayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrivingLog.Repository
+{
+  public class DataAccessLayer
+  {
+    MySqlConnection _data;
+    public DataAccessLayer()
+    {
+      _data = new MySqlConnection();
+    }
+
+    public DrivingLogDto SaveDrivingRecord(DrivingLogDto dto)
+    {
+      var id = _data.InsertDrivingRevord(dto);
+      dto.Id = id;
+      return dto;
+    }
+  }
+}
