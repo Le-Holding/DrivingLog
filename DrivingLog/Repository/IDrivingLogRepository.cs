76614f827a78db49a4efe9da00f7cb96530704a4
Mathias Casper Lynge Le-Holding <talamias@hotmail.com>
2021-01-18 11:08:12 +0100

    add interfaces

---
+++ DrivingLog/Repository/IDrivingLogRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrivingLog.Repository
+{
+  public interface IDrivingLogRepository
+  {
+    IList<EmployeeStamdataDto> GetDrivinglog();
+    EmployeeStamdataDto GetDrivinglogById(int? id);
+    void InsertDrivinglog(EmployeeStamdataDto dto);
+    void UpdateDrivinglog(EmployeeStamdataDto dto);
+    void DeleteDrivinglog(int id);
+  }
+}
