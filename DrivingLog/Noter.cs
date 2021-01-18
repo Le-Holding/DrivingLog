3d66525c4e1dc68ee1fdf444de7a94c8eb6ce5e4
Mathias Casper Lynge Le-Holding <talamias@hotmail.com>
2021-01-18 11:14:20 +0100

    add notes naming convetion - sql naming conventions

--- DrivingLog/Noter.cs
+++ DrivingLog/Noter.cs
@@ -25,5 +25,18 @@ namespace DrivingLog
     //https://www.infoworld.com/article/3109870/my-two-cents-on-deep-copy-vs-shallow-copy-in-net.html
     //https://stackoverflow.com/questions/14007405/how-create-a-new-deep-copy-clone-of-a-listt
     //https://stackoverflow.com/questions/6201529/how-do-i-turn-a-c-sharp-object-into-a-json-string-in-net
+
+
+    //Naming Conventions database objects 
+    //http://en.dwhwiki.info/templates/process/naming_conventions
+    //https://www.yiiframework.com/wiki/227/guidelines-for-good-schema-design
+    //https://stackoverflow.com/questions/3593582/database-naming-conventions-by-microsoft
+
+    //Hvorfor vi bruger snake case i databasen som naming convention
+    //https://stackoverflow.com/questions/16288586/any-reason-to-still-use-snake-case-for-database-tables-and-columns
+    //https://andrewlock.net/using-snake-case-column-names-with-dapper-and-postgresql/
+
+    //Naming conventions and sql joins 
+    //https://www.red-gate.com/simple-talk/blogs/sql-naming-conventions/
   }
 }
