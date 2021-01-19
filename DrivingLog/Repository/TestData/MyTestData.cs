b9c1c7b67a98cea9c6f603b0b335e620ab5b6646
Le-Holding <talamias@hotmail.com>
2021-01-19 13:43:13 +0100

    Extract test data to private property

--- DrivingLog/Repository/TestData/MyTestData.cs
+++ DrivingLog/Repository/TestData/MyTestData.cs
@@ -13,28 +13,40 @@ namespace DrivingLog.Repository.TestData
     {
       _bizz = new TestDataHelperClass();
     }
+    private List<EmployeeStamdataDto> Persons
+    {
+      get
+      {
+        //singleton Pattern
+        if (_persons == null) 
+          _persons = new List<EmployeeStamdataDto>()
+          {
+            new EmployeeStamdataDto { Id = 1, Name = "Jalina", LicensePlate = "CP86028", Date = new DateTime(2020, 08, 01) },
+            new EmployeeStamdataDto { Id = 2, Name = "Melina", LicensePlate = "AR86028", Date = new DateTime(2020, 01, 01) },
+            new EmployeeStamdataDto { Id = 3, Name = "Kalina", LicensePlate = "BK08022", Date = new DateTime(2019, 05, 25) },
+            new EmployeeStamdataDto { Id = 4, Name = "Selina", LicensePlate = "BA09455", Date = new DateTime(2015, 12, 30) }
+          };
+        
+        return _persons;
+      }
+      set => _persons = value;
+    }
+
     public List<EmployeeStamdataDto> GetPersons()
     {
-      var dtos = new List<EmployeeStamdataDto>()
-      {
-        new EmployeeStamdataDto { Id = 1, Name = "Jalina", LicensePlate = "CP86028", Date = new DateTime(2020, 08, 1) },
-        new EmployeeStamdataDto { Id = 2, Name = "Melina", LicensePlate = "AR86028", Date = new DateTime(2020, 01, 1) },
-        new EmployeeStamdataDto { Id = 3, Name = "Kalina", LicensePlate = "BK08022", Date = new DateTime(2019, 05, 25) },
-        new EmployeeStamdataDto { Id = 4, Name = "Selina", LicensePlate = "BA09455", Date = new DateTime(2015, 12, 30) }
-      };
-
-      foreach (var item in dtos)
+      foreach (var item in Persons)
       {
         item.KilometersPrTrip = _bizz.GetSubsetOfAListById(EmployeeDrivingLog, item.Id);
         item.kilometerSum = _bizz.GetSumOfProduct(item.KilometersPrTrip.Select(x => x.Distance).ToList());
         //item.kilometerSum = GetSumOfProduct(EmployeeDrivingLog.Where(x => x.EmployeeId == item.Id).Select(x => x.Distance).ToList());
       }
 
-      dtos.ForEach(x => x.DeepCopy = x.SetDeepCopy());
+      Persons.ForEach(x => x.DeepCopy = x.SetDeepCopy());
 
-      return dtos.OrderBy(x => x.Name).ToList();
+      return Persons.OrderBy(x => x.Name).ToList();
     }
 
+    private List<EmployeeStamdataDto> _persons;
     private List<DrivingLogDto> _employeeDrivingLog;
 
     public List<DrivingLogDto> EmployeeDrivingLog
@@ -42,21 +54,23 @@ namespace DrivingLog.Repository.TestData
       get
       {
         //singleton Pattern
-        if (_employeeDrivingLog == null) _employeeDrivingLog = new List<DrivingLogDto>()
-        {
-          new DrivingLogDto {Id =  1, EmployeeId = 1, Date = DateTime.UtcNow,  Distance = 10,   DriversTask = "TEC Hvidovre"},
-          new DrivingLogDto {Id =  2, EmployeeId = 1, Date = DateTime.Now,     Distance = 300,  DriversTask = "AU Aarhus"},
-          new DrivingLogDto { Id = 3, EmployeeId = 1, Date = DateTime.UtcNow, Distance = 96, DriversTask = "Tec Ballerup" },
-          new DrivingLogDto { Id = 4, EmployeeId = 2, Date = DateTime.Now, Distance = 10, DriversTask = "DTU Lyngby" },
-          new DrivingLogDto { Id = 5, EmployeeId = 2, Date = DateTime.UtcNow, Distance = 200, DriversTask = "DTU Aarhus" },
-          new DrivingLogDto { Id = 6, EmployeeId = 2, Date = DateTime.Now, Distance = 100, DriversTask = "Tec Ballerup" },
-          new DrivingLogDto { Id = 7, EmployeeId = 3, Date = DateTime.UtcNow, Distance = 400, DriversTask = "AAU AAlBorg" },
-          new DrivingLogDto { Id = 8, EmployeeId = 3, Date = DateTime.Now, Distance = 1398, DriversTask = "Gardasøen Italien" },
-          new DrivingLogDto { Id = 9, EmployeeId = 3, Date = DateTime.UtcNow, Distance = 1216, DriversTask = "Paris" },
-          new DrivingLogDto { Id = 10, EmployeeId = 4, Date = DateTime.Now, Distance = 1895, DriversTask = "Rom" },
-          new DrivingLogDto { Id = 11, EmployeeId = 4, Date = DateTime.UtcNow, Distance = 1895, DriversTask = "Tec Ballerup" },
-          new DrivingLogDto { Id = 12, EmployeeId = 4, Date = DateTime.Now, Distance = 3006, DriversTask = "Málaga" },
-        };
+        if (_employeeDrivingLog == null) 
+          _employeeDrivingLog = new List<DrivingLogDto>()
+          {
+            new DrivingLogDto {Id =  1, EmployeeId = 1, Date = DateTime.UtcNow,  Distance = 10,   DriversTask = "TEC Hvidovre"},
+            new DrivingLogDto {Id =  2, EmployeeId = 1, Date = DateTime.Now,     Distance = 300,  DriversTask = "AU Aarhus"},
+            new DrivingLogDto { Id = 3, EmployeeId = 1, Date = DateTime.UtcNow, Distance = 96, DriversTask = "Tec Ballerup" },
+            new DrivingLogDto { Id = 4, EmployeeId = 2, Date = DateTime.Now, Distance = 10, DriversTask = "DTU Lyngby" },
+            new DrivingLogDto { Id = 5, EmployeeId = 2, Date = DateTime.UtcNow, Distance = 200, DriversTask = "DTU Aarhus" },
+            new DrivingLogDto { Id = 6, EmployeeId = 2, Date = DateTime.Now, Distance = 100, DriversTask = "Tec Ballerup" },
+            new DrivingLogDto { Id = 7, EmployeeId = 3, Date = DateTime.UtcNow, Distance = 400, DriversTask = "AAU AAlBorg" },
+            new DrivingLogDto { Id = 8, EmployeeId = 3, Date = DateTime.Now, Distance = 1398, DriversTask = "Gardasøen Italien" },
+            new DrivingLogDto { Id = 9, EmployeeId = 3, Date = DateTime.UtcNow, Distance = 1216, DriversTask = "Paris" },
+            new DrivingLogDto { Id = 10, EmployeeId = 4, Date = DateTime.Now, Distance = 1895, DriversTask = "Rom" },
+            new DrivingLogDto { Id = 11, EmployeeId = 4, Date = DateTime.UtcNow, Distance = 1895, DriversTask = "Tec Ballerup" },
+            new DrivingLogDto { Id = 12, EmployeeId = 4, Date = DateTime.Now, Distance = 3006, DriversTask = "Málaga" },
+          };
+
         return _employeeDrivingLog;
       }
       set => _employeeDrivingLog = value;
