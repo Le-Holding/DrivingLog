fa63d567730ef5e64edd484d77d48819d1b1eaa8
Mathias Casper Lynge Le-Holding <mathiasholding.dk@gmail.com>
2021-01-23 23:10:40 +0100

    merge MyJoinSelectClass

--- DrivingLog/MyTestClass.cs
+++ DrivingLog/MyTestClass.cs
@@ -48,5 +48,11 @@ namespace DrivingLog
       System.Diagnostics.Debug.WriteLine(json);
       System.Diagnostics.Debug.WriteLine(temp);
     }
+
+    public static void MyJoinSelect()
+    {
+    //var myguery
+    //https://docs.microsoft.com/en-us/dotnet/api/system.web.script.serialization.javascriptserializer?redirectedfrom=MSDN&view=netframework-4.8
+    }
   }
 }
