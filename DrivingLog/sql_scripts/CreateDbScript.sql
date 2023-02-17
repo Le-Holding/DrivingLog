ad004193db61b6999a79879525583409de90ed7c
Mathias Le-Holding <68299863+Le-Holding@users.noreply.github.com>
2023-02-17 11:04:23 +0100

    add func create table employee

--- DrivingLog/sql_scripts/CreateDbScript.sql
+++ DrivingLog/sql_scripts/CreateDbScript.sql
@@ -2,13 +2,25 @@
 CREATE DATABASE [db_my_company];
 GO
 
+CREATE TABLE employee_card(
+    	[id] [int] IDENTITY(1,1) NOT NULL,
+        fullname VARCHAR(100) NOT NULL,
+        license_plate VARCHAR(10),
+        adresse VARCHAR(100),
+        zip_code SMALLINT,
+        phone SMALLINT,
+        mail VARCHAR(100),
+        create_date DATETIME NOT NULL,
+        edit_date DATETIME
+);
+Go
 
 CREATE TABLE [dbo].[employee_data_card](
 	[employee_id] [int] IDENTITY(1,1) NOT NULL,
 	[employee_name] [varchar(100)] NOT NULL,
 	[licen_plate] [varchar(30)] NOT NULL,
 	[employee_created] [datetime] NOT NULL,
-	[employee_corrected] [datetime] NULL
+	[employee_edited] [datetime] NULL,
 	CONSTRAINT [employee_pk] PRIMARY KEY CLUSTERED (employee_id)
 );
 
@@ -18,7 +30,7 @@ CREATE TABLE [dbo].[driving_record](
 	[kilometer_traveled] [int] NOT NULL,
 	[drived_date] [datetime] NOT NULL,
 	[driving_record_created] [datetime] NOT NULL,
-	[driving_record_corrected] [datetime] NULL
+	[driving_record_edited] [datetime] NULL
 	CONSTRAINT [driving_record_id_pk] PRIMARY KEY CLUSTERED (id),
 	CONSTRAINT [employee_employee_reiving_record] FOREIGN KEY REFERENCES employee_data_card(employee_id),
 	REFERENCES [dbo].[employee_data_card(employee_id)] ON DELETE CASCADE
